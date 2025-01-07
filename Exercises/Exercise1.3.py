6171d5c5e4335b846bf74961db2e6786811106af
O.Halaiev <o.halaiev@gmail.com>
2025-01-07 18:02:04 +0100

    Commit 2

--- Exercises/Exercise1.3.py
+++ Exercises/Exercise1.3.py
@@ -1,11 +1,11 @@
-def summ(a, b: float) -> float:
+def summ(a: float, b: float) -> float:
     return a + b
 
 user_input_1 = float(input("Enter the first number: "))
 user_input_2 = float(input("Enter the second number: "))
-result = round(summ(user_input_1, user_input_2), 5)
+result = summ(user_input_1, user_input_2)
 
-print(f"Sum of {user_input_1} and {user_input_2} = {result}")
+print(f"The sum of {user_input_1} and {user_input_2} is {result:.5f}")
 
 
 
--- Exercises/Exercise1.4.py
+++ Exercises/Exercise1.4.py
@@ -1,18 +1,11 @@
 import random
 
 def del_num_div(num_div_del: int) -> list:
-    # new_lst = []
-    # for num in random.randint[]:
-    #     if not num.isdigit():
-    #         print(f"Error: {num} - it's not a digit!")
-    #         break
-    #     elif not num // num_div_del:
-    #         new_lst.append(num)
-    # return new_lst
     if num_div_del == 0:
-        raise ZeroDivisionError("Error: you cannot division by zero!")
-    return [random.randint(1, 100) for i in range(random.randint(1, 100)) if not i // num_div_del]
-
+        raise ZeroDivisionError("Error: You cannot division by zero!")
+    numbers = list(range(1, 101))
+    filter_numbers = [num for num in numbers if num % num_div_del != 0]
+    return filter_numbers
 
 try:
     user_input = int(input("Enter a number: "))
@@ -20,7 +13,8 @@ try:
     print(f"Your list: {result}")
 except ZeroDivisionError as err:
     print(err)
-
+except ValueError:
+    print("Error: Please enter a valid number.")
 
 
 
